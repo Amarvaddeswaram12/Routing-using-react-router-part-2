4e817d0fa823aec76345c8c4a08ccfa5e539c3fa
Amarvaddeswaram12 <amar.vaddeswaram12@gmail.com>
2022-11-22 01:39:53 +0000

    routing using react router part-2

---
+++ src/components/BlogsList/index.js
@@ -0,0 +1,41 @@
+import {Component} from 'react'
+
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+class BlogsList extends Component {
+  state = {blogsData: []}
+
+  componentDidMount() {
+    this.getBlogsData()
+  }
+
+  getBlogsData = async () => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+    const data = await response.json()
+
+    const updatedData = data.map(eachItem => ({
+      id: eachItem.id,
+      title: eachItem.title,
+      imageUrl: eachItem.image_url,
+      avatarUrl: eachItem.avatar_url,
+      author: eachItem.author,
+      topic: eachItem.topic,
+    }))
+    this.setState({blogsData: updatedData})
+  }
+
+  render() {
+    const {blogsData} = this.state
+    return (
+      <div className="blog-list-container">
+        {blogsData.map(item => (
+          <BlogItem blogData={item} key={item.id} />
+        ))}
+      </div>
+    )
+  }
+}
+
+export default BlogsList
